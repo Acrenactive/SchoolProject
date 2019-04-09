a745852ca1f868b5af576b7cf942f3e6e190f167
Sener Nilsson <seners@live.se>
2019-04-09 15:31:59 +0200

    Update namn.js

--- namn.js
+++ namn.js
@@ -16,3 +16,13 @@ for (var n = namn.length-1; n>=0; n--)
 }
 document.body.innerHTML += " <> " +namn.toUpperCase();
 
+
+
+
+   <script>
+        var nr = ["noll","ett","två","tre","fyra"];
+        var mobil = prompt("Mobil nr: ");
+        document.body.innerHTML += mobil[0]+"<br>";
+        document.body.innerHTML += nr[mobil[0]]+"<br>";
+     </script>   
+</body>
